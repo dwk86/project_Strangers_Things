bc1ecf34724bb224e098df609d9424dbd1f9e8e7
Dan_Kempert <dwkempert@gmail.com>
2021-12-10 20:54:17 -0500

    added all messages view on 'view my posts' view

--- src/components/MyPosts.js
+++ src/components/MyPosts.js
@@ -6,6 +6,7 @@ const MyPosts = (props) => {
     let userToken = props.userToken
 
     let [ myPosts, setMyPosts ] = useState([])
+    let [ myMessages, setMyMessages ] = useState([])
     let [ viewComments, setViewComments ] = useState(false)
     let [ viewPostCommentsId, setViewPostCommentsId ] = useState("")
 
@@ -15,6 +16,7 @@ const MyPosts = (props) => {
                 const data = await fetchMyPosts(userToken)
                 console.log("myPosts data:", data.data.posts)
                 setMyPosts(data.data.posts)
+                setMyMessages(data.data.messages)
             } catch (error) {
                 console.error("Something went wrong fetching my posts!", error)
             }
@@ -26,44 +28,57 @@ const MyPosts = (props) => {
         <div id="myPosts" className="mainContent">You must be logged in or no data has been found!</div>
         :
         <div id="myPosts" className="mainContent">
-            {myPosts ? myPosts.map((el, index) => (
-                el.active ?
-                    <div key={index} className="myPostElement">
-                        <h2 className="myPostTitle">{el.title}</h2>
-                        <div className="myPostDescription">{el.description}</div>
-                        <h3 className="myPostPrice">{el.price} - {el.willDeliver ? "Will Deliver" : "Will NOT Deliver"}</h3>
-                        <div className="myPostsOptions">
-                            {/* <button id="myPostsEdit">Edit Post</button> */}
-                            <button className="myPostsViewComments" onClick={() => {
-                                if (viewComments) {
-                                    setViewComments(false)
-                                } else {
-                                    setViewComments(true)
-                                }
-                                setViewPostCommentsId(el._id)
-                            }}>View Messages</button>
-                            <button className="myPostsDeletePost" onClick={() => {
-                                async function runDelete(postId, userToken) {
-                                    try {
-                                        const result = await deleteMyPost(postId, userToken)
-                                    } catch (error) {
-                                        console.error("Something went wrong deleting my post!", error)
+            <div id="postArea">
+                <h2>All Posts:</h2>
+                {myPosts ? myPosts.map((el, index) => (
+                    el.active ?
+                        <div key={index} className="myPostElement">
+                            <h2 className="myPostTitle">{el.title}</h2>
+                            <div className="myPostDescription">{el.description}</div>
+                            <h3 className="myPostPrice">{el.price} - {el.willDeliver ? "Will Deliver" : "Will NOT Deliver"}</h3>
+                            <div className="myPostsOptions">
+                                {/* <button id="myPostsEdit">Edit Post</button> */}
+                                <button className="myPostsViewComments" onClick={() => {
+                                    if (viewComments) {
+                                        setViewComments(false)
+                                    } else {
+                                        setViewComments(true)
                                     }
-                                }
-                                runDelete(el._id, userToken)
-                            }}>Delete Post</button>
+                                    setViewPostCommentsId(el._id)
+                                }}>View Messages</button>
+                                <button className="myPostsDeletePost" onClick={() => {
+                                    async function runDelete(postId, userToken) {
+                                        try {
+                                            const result = await deleteMyPost(postId, userToken)
+                                        } catch (error) {
+                                            console.error("Something went wrong deleting my post!", error)
+                                        }
+                                    }
+                                    runDelete(el._id, userToken)
+                                }}>Delete Post</button>
+                            </div>
+                            {viewPostCommentsId == el._id ?
+                                viewComments ? el.messages.map((elem, i) => (
+                                    <div key={i+"a"}>
+                                        <div key={i+"b"}>{elem.fromUser.username}</div>
+                                        <div key={i+"c"}>{elem.content}</div>
+                                    </div>
+                                )) : undefined
+                            : undefined }
                         </div>
-                        {viewPostCommentsId == el._id ?
-                            viewComments ? el.messages.map((elem, i) => (
-                                <div key={i+"a"}>
-                                    <div key={i+"b"}>{elem.fromUser.username}</div>
-                                    <div key={i+"c"}>{elem.content}</div>
-                                </div>
-                            )) : undefined
-                        : undefined }
+                        : undefined
+                )) : "No Data Found!"}
+            </div>
+            <div id="allMessagesArea">
+                <h2>All Messages:</h2>
+                {myMessages ? myMessages.map((element, index) => (
+                    <div key={index} className="myMessageElement">
+                        <h3 className="myMessageFromUser">Message from: {element.fromUser.username}</h3>
+                        <div className="myMessagePostTitle">message posted on: {element.post.title}</div>
+                        <div className="myMessageContent">{element.content}</div>
                     </div>
-                    : undefined
-            )) : "No Data Found!"}
+                )) : "No Messages Found!" }
+            </div>
         </div>
     )
 }
